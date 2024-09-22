c07d4a215635dba517dcc054685732bc10b93a78
KaranRohra <rohrakaran38@gmail.com>
2024-09-22 11:16:47 +0530

    Add middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { Endpoints, Methods } from "./api/constants";
+import UI_PATH from "./constants/ui-path-constants";
+import { apiCall } from "./utils/utils-server";
+
+const protectedRoutes = [UI_PATH.PROFILE];
+
+export default async function middleware(req: NextRequest) {
+  const user = await apiCall({
+    method: Methods.GET,
+    url: Endpoints.USER,
+  });
+  if (user.status !== 200 && protectedRoutes.includes(req.nextUrl.pathname as any)) {
+    const absoluteURL = new URL(UI_PATH.LOGIN, req.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+}
